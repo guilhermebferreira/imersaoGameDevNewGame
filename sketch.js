5fbbe147c7a7000bec6c53e6a85ec663a109b2c3
guilhermebferreira <guilherme@mail.uft.edu.br>
2020-06-22 22:10:12 -0300

    [UPDATE] desenhando os assets na tela

--- sketch.js
+++ sketch.js
@@ -1,8 +1,18 @@
+let imagemCenario;
+let imagemPersonagem;
+
+function preload(){
+  imagemCenario = loadImage('imagens/cenario/floresta.png');
+  imagemPersonagem = loadImage('imagens/personagem/correndo.png');
+}
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(50,200,200);
+  background(imagemCenario);
+  image(imagemPersonagem,mouseX,height-150, 110, 135, 0,0, 220,270);
   circle(mouseX,mouseY,50);
 }
