5244e269eda6b1d59f47c7ac01221334d918aaa8
guilhermebferreira <guilherme@mail.uft.edu.br>
2020-06-22 22:52:04 -0300

    [UPDATE] criando class cenario

--- sketch.js
+++ sketch.js
@@ -9,11 +9,40 @@ function preload(){
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  cenario = new Cenario(imagemCenario, 3);
 }
 
 function draw() {
-  image(imagemCenario, -mouseX, 0, width, height);
-  image(imagemCenario, width-mouseX, 0, width, height);
+  cenario.exibe();
   image(imagemPersonagem,mouseX,height-150, 110, 135, 0,0, 220,270);
   circle(mouseX,mouseY,50);
+  
+  cenario.move();
+}
+
+class Cenario{
+  constructor(imagem, velocidade){
+    this.imagem = imagem;
+    this.velocidade = velocidade;
+    this.x1 = 0;
+    this.x2 = width;
+  }
+  exibe(){
+    image(this.imagem, this.x1, 0, width, height);
+    image(this.imagem, this.x2 +1, 0, width, height);
+  }
+  move(){
+    this.x1 = this.x1 - this.velocidade;
+    this.x2 = this.x2 - this.velocidade;
+    if(this.x1 < -width){
+      this.x1 = width;
+    }
+      
+    if(this.x2 < -width){
+      this.x2 = width;
+    }
+      
+  }
+    
+  
 }
