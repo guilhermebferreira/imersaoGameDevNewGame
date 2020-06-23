037e65d5464bb5b0db91d752d4415b375bd80a93
guilhermebferreira <guilherme@mail.uft.edu.br>
2020-06-22 22:19:34 -0300

    [UPDATE] movendo o background dinamicamente

--- sketch.js
+++ sketch.js
@@ -12,7 +12,8 @@ function setup() {
 }
 
 function draw() {
-  background(imagemCenario);
+  image(imagemCenario, -mouseX, 0, width, height);
+  image(imagemCenario, width-mouseX, 0, width, height);
   image(imagemPersonagem,mouseX,height-150, 110, 135, 0,0, 220,270);
   circle(mouseX,mouseY,50);
 }
