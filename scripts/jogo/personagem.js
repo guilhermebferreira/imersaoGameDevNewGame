0d45cb48361d380cb50a4073dc73ee8ab1c304c5
guilhermebferreira <guilherme@mail.uft.edu.br>
2020-06-22 23:03:08 -0300

    [UPDATE] animando a personagem

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,11 +1,36 @@
 class Personagem{
   constructor(image){
-    this.imagem = imagem;
+    this.imagem = image;
+    this.matriz = [
+            [0, 0],
+            [220, 0],
+            [440, 0],
+            [660, 0],
+            [0, 270],
+            [220, 270],
+            [440, 270],
+            [660, 270],
+            [0, 540],
+            [220, 540],
+            [440, 540],
+            [660, 540],
+            [0, 810],
+            [220, 810],
+            [440, 810],
+            [660, 810],
+        ] 
+    this.frameAtual = 0;
   }
   
   exibe(){
-      image(imagem,mouseX,height-150, 110, 135, 0,0, 220,270);
-
+      image(this.imagem,mouseX,height-150, 110, 135, this.matriz[this.frameAtual][0],this.matriz[this.frameAtual][1], 220,270);
+this.anima();
+  }
+  anima(){
+      this.frameAtual++;
+    if(this.frameAtual > 16){
+      this.frameAtual = 0;
+    }
   }
   
 }
--- sketch.js
+++ sketch.js
@@ -1,48 +1,26 @@
 let imagemCenario;
 let imagemPersonagem;
+let soundtrack;
 
 function preload(){
   imagemCenario = loadImage('imagens/cenario/floresta.png');
   imagemPersonagem = loadImage('imagens/personagem/correndo.png');
+  soundtrack = loadSound('sons/trilha_jogo.mp3');
 }
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario = new Cenario(imagemCenario, 3);
+  personagem = new Personagem(imagemPersonagem);
+  frameRate(30)
+  
+  soundtrack.loop();
 }
 
 function draw() {
   cenario.exibe();
-  image(imagemPersonagem,mouseX,height-150, 110, 135, 0,0, 220,270);
-  circle(mouseX,mouseY,50);
-  
   cenario.move();
-}
-
-class Cenario{
-  constructor(imagem, velocidade){
-    this.imagem = imagem;
-    this.velocidade = velocidade;
-    this.x1 = 0;
-    this.x2 = width;
-  }
-  exibe(){
-    image(this.imagem, this.x1, 0, width, height);
-    image(this.imagem, this.x2 +1, 0, width, height);
-  }
-  move(){
-    this.x1 = this.x1 - this.velocidade;
-    this.x2 = this.x2 - this.velocidade;
-    if(this.x1 < -width){
-      this.x1 = width;
-    }
-      
-    if(this.x2 < -width){
-      this.x2 = width;
-    }
-      
-  }
-    
-  
+  personagem.exibe();
+  circle(mouseX,mouseY,50);
 }
