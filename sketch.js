9207daef90a441236a807b98ec1b14f6a00f29db
guilhermebferreira <guilherme@mail.uft.edu.br>
2020-06-25 22:09:13 -0300

    [UPDATE] adicionando outro inimigo, e ativando pulo tbm com a barra de espaço

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let imagemCenario;
 let imagemPersonagem;
+let imagemInimigoGrande;
 let imagemGameOver;
 let soundtrack;
 let soundPulo;
@@ -32,7 +33,7 @@ const matrizInimigo = [
     [104, 626],
     [208, 626],
     [312, 626],
-]
+];
 
 
 const matrizPersonagem = [
@@ -52,13 +53,65 @@ const matrizPersonagem = [
     [220, 810],
     [440, 810],
     [660, 810],
-]
+];
+
+const matrizInimigoGrande = [
+    [0,0],
+    [400,0],
+    [800,0],
+    [1200,0],
+    [1600,0],
+    [0,400],
+    [400,400],
+    [800,400],
+    [1200, 400],
+    [1600, 400],
+    [0,800],
+    [400, 800],
+    [800, 800],
+    [1200, 800],
+    [1600, 800],
+    [0, 1200],
+    [400, 1200],
+    [800, 1200],
+    [1200, 1200],
+    [1600, 1200],
+    [0, 1600],
+    [400, 1600],
+    [800, 1600],
+    [1200, 1600],
+    [1600, 1600],
+    [0, 2000],
+    [400, 2000],
+    [800, 2000],
+];
+
+const matrizInimigoVoador = [
+    [0,0],
+    [200, 0],
+    [400, 0],
+    [0, 150],
+    [200, 150],
+    [400, 150],
+    [0, 300],
+    [200, 300],
+    [400, 300],
+    [0, 450],
+    [200, 450],
+    [400, 450],
+    [0, 600],
+    [200, 600],
+    [400, 600],
+    [0, 750],
+];
 
 function preload() {
     imagemCenario = loadImage('imagens/cenario/floresta.png');
     imagemGameOver = loadImage('imagens/assets/gameover.png');
     imagemPersonagem = loadImage('imagens/personagem/correndo.png');
     imagemInimigo = loadImage('imagens/inimigos/gotinha.png');
+    imagemInimigoGrande = loadImage('imagens/inimigos/troll.png');
+    imagemVoador = loadImage('imagens/inimigos/gotinha-voadora.png');
     soundtrack = loadSound('sons/trilha_jogo.mp3');
     soundPulo = loadSound('sons/somPulo.mp3');
 
@@ -71,13 +124,15 @@ function setup() {
     cenario = new Cenario(imagemCenario, 3);
     personagem = new Personagem(matrizPersonagem, imagemPersonagem, 0, 0,110, 135, 220, 270);
     inimigo = new Inimigo(matrizInimigo, imagemInimigo, width - 52, 0,52, 52, 104, 104);
+    inimigoGrande = new Inimigo(matrizInimigoGrande, imagemInimigoGrande, width + 500, 0, 200,200,400,400);
     frameRate(40);
 
     soundtrack.loop();
 }
 
 function keyPressed() {
-    if (key === 'ArrowUp') {
+    console.log(key);
+    if (key === 'ArrowUp' || key === ' ' ) {
         personagem.pula();
         soundPulo.play();
     }
@@ -108,6 +163,12 @@ function draw() {
     personagem.aplicaGravidade();
     inimigo.exibe();
     inimigo.move();
+
+    inimigoGrande.exibe();
+    inimigoGrande.move();
+
+
+
     circle(mouseX, mouseY, 50);
 
     debug();
